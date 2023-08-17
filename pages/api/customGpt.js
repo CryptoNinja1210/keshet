6db8bd61776ea36fe59499cd44d065088fb34786
CrryptoNinja0617 <crypto.ninja1210@gmail.com>
2023-08-18 05:41:21 +0900

    disable

--- pages/api/customGpt.js
+++ pages/api/customGpt.js
@@ -1,91 +1,91 @@
-//documents loaders for differents files formats
-import {DirectoryLoader} from "langchain/document_loaders/fs/directory";
-import {JSONLoader} from "langchain/document_loaders/fs/json";
-import {TextLoader} from "langchain/document_loaders/fs/text";
-import {PDFLoader} from "langchain/document_loaders/fs/pdf";
-import {CSVLoader} from "langchain/document_loaders/fs/csv";
+// //documents loaders for differents files formats
+// import {DirectoryLoader} from "langchain/document_loaders/fs/directory";
+// import {JSONLoader} from "langchain/document_loaders/fs/json";
+// import {TextLoader} from "langchain/document_loaders/fs/text";
+// import {PDFLoader} from "langchain/document_loaders/fs/pdf";
+// import {CSVLoader} from "langchain/document_loaders/fs/csv";
 
-//open ai llm and other elated modules
-import { OpenAI } from "langchain/llms/openai";
-import {RetrievalQAChain} from "langchain/chains";
-import { ConversationalRetrievalQAChain } from "langchain/chains";
-import {BufferMemory} from 'langchain/memory';
-import {HNSWLib} from "langchain/vectorstores/hnswlib";
-import {OpenAIEmbeddings} from "langchain/embeddings/openai";
-import {RecursiveCharacterTextSplitter} from "langchain/text_splitter";
+// //open ai llm and other elated modules
+// import { OpenAI } from "langchain/llms/openai";
+// import {RetrievalQAChain} from "langchain/chains";
+// import { ConversationalRetrievalQAChain } from "langchain/chains";
+// import {BufferMemory} from 'langchain/memory';
+// import {HNSWLib} from "langchain/vectorstores/hnswlib";
+// import {OpenAIEmbeddings} from "langchain/embeddings/openai";
+// import {RecursiveCharacterTextSplitter} from "langchain/text_splitter";
 
-// Import Tiktoken for token counting
-import { Tiktoken } from "@dqbd/tiktoken/lite";
-import { load } from "@dqbd/tiktoken/load";
-import registry from "@dqbd/tiktoken/registry.json" assert { type: "json" };
-import models from "@dqbd/tiktoken/model_to_encoding.json" assert { type: "json" };
+// // Import Tiktoken for token counting
+// import { Tiktoken } from "@dqbd/tiktoken/lite";
+// import { load } from "@dqbd/tiktoken/load";
+// import registry from "@dqbd/tiktoken/registry.json" assert { type: "json" };
+// import models from "@dqbd/tiktoken/model_to_encoding.json" assert { type: "json" };
 
-//Import dotenv for loading environment variables and fs for file system operations
-import readline from "readline";
-import dotenv from 'dotenv';
-import fs from "fs";
-import path from "path";
-dotenv.config({path: "./.env.local"});
+// //Import dotenv for loading environment variables and fs for file system operations
+// import readline from "readline";
+// import dotenv from 'dotenv';
+// import fs from "fs";
+// import path from "path";
+// dotenv.config({path: "./.env.local"});
 
-//initazllize memory
-const memory = new BufferMemory({
-  memoryKey: "chat_history",
-  inputKey: "question", // The key for the input to the chain
-  outputKey: "text", // The key for the final conversational output of the chain
-  returnMessages: true, // If the chat model should return the messages
-});
-
-
-
-//Initialize the document loader with supported file formats
-const loader = new DirectoryLoader("./documents", {
-    ".json": (path) => new JSONLoader(path),
-    ".text": (path) => new TextLoader(path),
-    ".csv": (path) => new CSVLoader(path),
-    ".pdf": (path) => new PDFLoader(path),
-});
-
-//Load documents from the specified directory
-console.log("loading documents");
-const docs = await loader.load();
-console.log("documents have been loaded successfully")
+// //initazllize memory
+// const memory = new BufferMemory({
+//   memoryKey: "chat_history",
+//   inputKey: "question", // The key for the input to the chain
+//   outputKey: "text", // The key for the final conversational output of the chain
+//   returnMessages: true, // If the chat model should return the messages
+// });
 
 
-//efine a function to calculate the cost of tokenizing the documents
-async function calculateCost() {
-    const modelName = "text-embedding-ada-002";
-    const modelKey = models[modelName];
-    const model = await load(registry[modelKey]);
-    const encoder = new Tiktoken(
-      model.bpe_ranks,
-      model.special_tokens,
-      model.pat_str
-    );
-    const tokens = encoder.encode(JSON.stringify(docs));
-    const tokenCount = tokens.length;
-    const ratePerThousandTokens = 0.0004;
-    const cost = (tokenCount / 1000) * ratePerThousandTokens;
-    encoder.free();
-    return cost;
-  }
 
-  const VECTOR_STORE_PATH = "Documents.index";
-  const question = "what is the most important thing in homeopathy according the docs";
+// //Initialize the document loader with supported file formats
+// const loader = new DirectoryLoader("./documents", {
+//   ".json": (path) => new JSONLoader(path),
+//   ".text": (path) => new TextLoader(path),
+//   ".csv": (path) => new CSVLoader(path),
+//   ".pdf": (path) => new PDFLoader(path),
+// });
 
- //Define a function to normalize the content of the documents
-function normalizeDocuments(docs) {
-  return docs.map((doc) => {
-    if (typeof doc.pageContent === "string") {
-      return doc.pageContent;
-    } else if (Array.isArray(doc.pageContent)) {
-      return doc.pageContent.join("\n");
-    }
-  });
-}
+// //Load documents from the specified directory
+// console.log("loading documents");
+// const docs = await loader.load();
+// console.log("documents have been loaded successfully")
 
-export default async function handler(res, req) {
-  console.log('end')
-}
+
+// //efine a function to calculate the cost of tokenizing the documents
+// async function calculateCost() {
+//     const modelName = "text-embedding-ada-002";
+//     const modelKey = models[modelName];
+//     const model = await load(registry[modelKey]);
+//     const encoder = new Tiktoken(
+//       model.bpe_ranks,
+//       model.special_tokens,
+//       model.pat_str
+//     );
+//     const tokens = encoder.encode(JSON.stringify(docs));
+//     const tokenCount = tokens.length;
+//     const ratePerThousandTokens = 0.0004;
+//     const cost = (tokenCount / 1000) * ratePerThousandTokens;
+//     encoder.free();
+//     return cost;
+//   }
+
+//   const VECTOR_STORE_PATH = "Documents.index";
+//   const question = "what is the most important thing in homeopathy according the docs";
+
+//  //Define a function to normalize the content of the documents
+// function normalizeDocuments(docs) {
+//   return docs.map((doc) => {
+//     if (typeof doc.pageContent === "string") {
+//       return doc.pageContent;
+//     } else if (Array.isArray(doc.pageContent)) {
+//       return doc.pageContent.join("\n");
+//     }
+//   });
+// }
+
+// export default async function handler(res, req) {
+//   console.log('end')
+// }
 
 // //main function to run the process
 // export const run = async (acceptableCost) => {
