2d87d245e01260aea0149b2df733322bd413ef5a
CrryptoNinja0617 <crypto.ninja1210@gmail.com>
2023-08-18 10:08:01 +0900

    env

--- app/components/chat/Chat.tsx
+++ app/components/chat/Chat.tsx
@@ -91,40 +91,40 @@ const url = '/api/homeophaticBot';
         ))}
         {messages.length === 0 && <p className='text-center text-gray-400'>I am at youre service</p>}
       </div>
-        <iframe
-          style={{border: 'none'}}
-          srcDoc="
-            <body>
-              <script src='https://cdn.botpress.cloud/webchat/v0/inject.js'></script>
-              <script>
-                window.botpressWebChat.init({
-                    'composerPlaceholder': 'Chat with bot',
-                    'botConversationDescription': 'This chatbot was built surprisingly fast with Botpress',
-                    'botName': 'Name',
-                    'botId': 'f87c4530-e481-49dd-8841-236d83aa1389',
-                    'hostUrl': 'https://cdn.botpress.cloud/webchat/v0',
-                    'messagingUrl': 'https://messaging.botpress.cloud',
-                    'clientId': 'f87c4530-e481-49dd-8841-236d83aa1389',
-                    'enableConversationDeletion': true,
-                    'showPoweredBy': true,
-                    'className': 'webchatIframe',
-                    'containerWidth': '100%25',
-                    'layoutWidth': '100%25',
-                    'hideWidget': true,
-                    'showCloseButton': false,
-                    'disableAnimations': true,
-                    'closeOnEscape': false,
-                    'showConversationsButton': false,
-                    'enableTranscriptDownload': false,
-                    'stylesheet':'https://webchat-styler-css.botpress.app/prod/code/3fcd3e4e-d5bc-4bf5-8699-14b621b3ada2/v31782/style.css'
-                });
+      <iframe
+        style={{border: 'none'}}
+        srcDoc="
+          <body>
+            <script src='https://cdn.botpress.cloud/webchat/v0/inject.js'></script>
+            <script>
+              window.botpressWebChat.init({
+                'composerPlaceholder': 'Chat with keshet',
+                'botConversationDescription': 'This chatbot was built surprisingly fast with Botpress',
+                'botName': 'Name',
+                'botId': 'e5855662-da48-4755-b0df-23844a98647c',
+                'hostUrl': 'https://cdn.botpress.cloud/webchat/v0',
+                'messagingUrl': 'https://messaging.botpress.cloud',
+                'clientId': 'e5855662-da48-4755-b0df-23844a98647c',
+                'enableConversationDeletion': true,
+                'showPoweredBy': true,
+                'className': 'webchatIframe',
+                'containerWidth': '100%25',
+                'layoutWidth': '100%25',
+                'hideWidget': true,
+                'showCloseButton': false,
+                'disableAnimations': true,
+                'closeOnEscape': false,
+                'showConversationsButton': false,
+                'enableTranscriptDownload': false,
+                'stylesheet':'https://webchat-styler-css.botpress.app/prod/code/3fcd3e4e-d5bc-4bf5-8699-14b621b3ada2/v31782/style.css'
+              });
               window.botpressWebChat.onEvent(function () { window.botpressWebChat.sendEvent({ type: 'show' }) }, ['LIFECYCLE.LOADED']);
-              </script>
-            </body>
-          "
-          width={'100%'}
-          height={700}
-        >
+            </script>
+          </body>
+        "
+        width={'100%'}
+        height={600}
+      >
       </iframe>
     </main>
   );
